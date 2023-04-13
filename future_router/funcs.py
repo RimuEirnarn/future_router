cd6b881952558b3a1f9222e84d00fb284b247ab2
RimuEirnarn <rimuru720@proton.me>
2023-04-13 14:09:06 +0700

    Bugfix: undefined method after using notimplemented

--- future_router/funcs.py
+++ future_router/funcs.py
@@ -2,3 +2,4 @@
 def notimplemented(func):
     """Set route method as Not Implemented, this would make sure that certain function is striped."""
     setattr(func, '_notimplemented', True)
+    return func
